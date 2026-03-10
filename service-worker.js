dda1512c54c49783a039b822db8c7307ee289652
saidelhabal406-cpu <saidelhabal406@gmail.com>
2026-03-10 00:02:10 +0000

    Delete service-worker.js

--- service-worker.js
+++
@@ -1,7 +0,0 @@
-self.addEventListener('install', (event) => {
-    console.log('Service Worker: Installed');
-});
-
-self.addEventListener('fetch', (event) => {
-    event.respondWith(fetch(event.request).catch(() => caches.match(event.request)));
-});
